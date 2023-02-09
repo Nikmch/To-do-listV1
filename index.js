a9fe6e7fa89f17f211fb9b07358ce5fba0c7a8db
Nikmch <107665487+Nikmch@users.noreply.github.com>
2023-02-09 20:21:38 +1100

    update list path

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const _ = require("lodash");
 
 const port = process.env.PORT || 3000;
 const app = express();
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
